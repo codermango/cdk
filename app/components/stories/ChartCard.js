fe313fab51107bcf526438081e7abf528b13eeaf
mark <ke@vionlabs.com>
2016-08-17 15:18:53 +0200

    Somehow refact chartcard styles

--- app/components/stories/ChartCard.js
+++ app/components/stories/ChartCard.js
@@ -8,33 +8,46 @@ import { storiesOf } from '@kadira/storybook';
 
 storiesOf('ChartCard', module)
   .add('default view', () => (
-    <div>
+    <div style={{ display: 'flex' }}>
+      <ChartCard />
       <ChartCard />
     </div>
   ))
   .add('AreaChart view', () => (
-    <div>
+    <div style={{ display: 'flex' }}>
+      <ChartCard height={200}>
+        <AreaChart />
+      </ChartCard>
       <ChartCard height={200}>
         <AreaChart />
       </ChartCard>
     </div>
   ))
   .add('BarChart view', () => (
-    <div>
+    <div style={{ display: 'flex' }}>
+      <ChartCard height={200}>
+        <BarChart />
+      </ChartCard>
       <ChartCard height={200}>
         <BarChart />
       </ChartCard>
     </div>
   ))
   .add('LineChart view', () => (
-    <div>
+    <div style={{ display: 'flex' }}>
+      <ChartCard height={200}>
+        <LineChart />
+      </ChartCard>
       <ChartCard height={200}>
         <LineChart />
       </ChartCard>
     </div>
   ))
   .add('StackedLineChart view', () => (
-    <div>
+    <div style={{ display: 'flex' }}>
+      <ChartCard height={200}>
+        <StackedLineChart />
+      </ChartCard>
       <ChartCard height={200}>
         <StackedLineChart />
       </ChartCard>
