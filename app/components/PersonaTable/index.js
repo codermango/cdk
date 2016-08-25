604ffb1234f07b975717aeeb203750029ebe9580
mark <ke@vionlabs.com>
2016-08-25 14:56:44 +0200

    Persona table rows can be sorted, but the implementation is ugly now...

--- app/components/PersonaTable/index.js
+++ app/components/PersonaTable/index.js
@@ -11,21 +11,56 @@ import PersonaRow from '../PersonaRow';
 
 import styles from './styles.css';
 
-function PersonaTable(props) {
-  const { data } = props;
-  return (
-    <div className={styles.personaTable}>
-      <PersonaTableHeader />
-      <PersonaRow className={styles.personaRow} data={data} />
-      <PersonaRow className={styles.personaRow} data={data} />
-      <PersonaRow className={styles.personaRow} data={data} />
-      <PersonaRow className={styles.personaRow} data={data} />
-    </div>
-  );
-}
+class PersonaTable extends React.Component {
 
-PersonaTable.propTypes = {
-  data: React.PropTypes.object,
-};
+  static propTypes = {
+    data: React.PropTypes.array,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      sortedData: this.props.data.sort((x, y) => x.size < y.size),
+    };
+    this.sortRow = this.sortRow.bind(this);
+  }
+
+  sortRow(sortBy, order) {
+    let sortFunc = '';
+    if (sortBy === 'size') {
+      sortFunc = order === 'asc' ? (x, y) => x[sortBy] > y[sortBy] : (x, y) => x[sortBy] < y[sortBy];
+    } else if (sortBy === 'uniqueViewers' || sortBy === 'avgFinishedMovies') {
+      sortFunc = order === 'asc' ?
+        (x, y) => x[sortBy].number > y[sortBy].number
+        :
+        (x, y) => x[sortBy].number < y[sortBy].number;
+    } else if (sortBy === 'avgViewingTime') {
+      sortFunc = order === 'asc' ?
+        (x, y) => (x[sortBy].min * 60) + x[sortBy].sec > (y[sortBy].min * 60) + y[sortBy].sec
+        :
+        (x, y) => (x[sortBy].min * 60) + x[sortBy].sec < (y[sortBy].min * 60) + y[sortBy].sec;
+    }
+
+    this.setState({
+      sortedData: this.props.data.sort(sortFunc),
+    });
+  }
+
+  render() {
+    // const { data } = this.props;
+    return (
+      <div className={styles.personaTable}>
+        <PersonaTableHeader sortRow={this.sortRow} />
+        {this.state.sortedData ?
+          this.state.sortedData.map((rowData, i) => (
+            <PersonaRow key={i} className={styles.personaRow} data={rowData} />
+          ))
+          :
+          ''
+        }
+      </div>
+    );
+  }
+}
 
 export default PersonaTable;
